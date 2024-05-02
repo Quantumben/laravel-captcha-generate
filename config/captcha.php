8bcaf5cad255c7b8a5146b6049ce8cd4ebb06330
Benedict Goodluck <omosabenedict@gmail.com>
2024-05-02 20:37:11 +0100

    Tweaked some configuration

--- config/captcha.php
+++ config/captcha.php
@@ -4,13 +4,20 @@ return [
     'disable' => env('CAPTCHA_DISABLE', false),
     'characters' => ['2', '3', '4', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'm', 'n', 'p', 'q', 'r', 't', 'u', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'X', 'Y', 'Z'],
     'default' => [
-        'length' => 9,
-        'width' => 120,
-        'height' => 36,
-        'quality' => 90,
-        'math' => false,
-        'expire' => 60,
-        'encrypt' => false,
+        // 'length' => 9,
+        // 'width' => 120,
+        // 'height' => 36,
+        // 'quality' => 90,
+        // 'math' => false,
+        // 'expire' => 60,
+        // 'encrypt' => false,
+
+        'length'    => 5,
+        'width'     => 120,
+        'height'    => 36,
+        'quality'   => 90,
+        'math'      => true, //Enable Math Captcha
+        'expire'    => 60,   //Stateless/API captcha expiration
     ],
     'math' => [
         'length' => 9,
