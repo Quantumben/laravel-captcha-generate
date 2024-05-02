57137ef724cb3c4b745289d53e6b41e535bf870c
Benedict Goodluck <omosabenedict@gmail.com>
2024-05-02 20:58:51 +0100

    captcha working perfectly

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CaptchaServiceController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [CaptchaServiceController::class, 'index']);
+Route::get('/contact-form', [CaptchaServiceController::class, 'index']);
+Route::post('/captcha-validation', [CaptchaServiceController::class, 'capthcaFormValidate']);
+Route::get('/reload-captcha', [CaptchaServiceController::class, 'reloadCaptcha']);
